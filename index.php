aa47484b6893fcb053be2b345610a9c618a8fb5a
rimanaru <rima.naruseviciute@gmail.com>
2018-02-06 23:18:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+$name = null;
+
+ function name( $name){ ?>
+<h1><?php echo $name; ?></h1>
+<?php } 
+
+name("Rima");?>
+
+<?php 
+
+
+
+function twoVar (String $first) {
+if($first == true){?>
+    
+    <h1><?php echo $first; ?></h1>
+    <?php
+}
+else if($first !== true){
+    echo( $first);
+}
+else{
+    return $first;
+}
+
+}
+twoVar("vardas");
+
+$hapy = " :)";
+$sad = ":(";
+
+function emotion($emo){
+    //If($emo = )
+
+}
+
+emotion()
+?>
+
+
+
+
+
