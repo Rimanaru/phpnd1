3d6ebc6c1a1b690745d5d1531a013961a12a27f6
rimanaru <rima.naruseviciute@gmail.com>
2018-02-07 15:33:23 +0200

    second commit

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
-<?php
-declare(strict_types = 1);
-$name = null;
+<?php declare(strict_types = 1);?> 
+
+<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+<?php $name = null;
 
  function name( $name){ ?>
 <h1><?php echo $name; ?></h1>
 <?php } 
 
-name("Rima");?>
-
-<?php 
+name("Rima");
 
 
-
-function twoVar (String $first) {
+function twoVar ( String $first) {
 if($first == true){?>
     
     <h1><?php echo $first; ?></h1>
@@ -21,25 +19,53 @@ if($first == true){?>
 else if($first !== true){
     echo( $first);
 }
-else{
-    return $first;
+else if ($first == "" ){
+
+    return false;
+    
 }
 
 }
-twoVar("vardas");
+ twoVar("vardas");
 
-$hapy = " :)";
-$sad = ":(";
+ //$name = "vardenis";
+ function newName (  $name) {
+    if($name == "vardenis"){?>
+        
+        <h1><?php echo $name; ?></h1>
+        <?php
+    }
+    else if($name !== "vardenis"){
+        echo( $name) ."<br>";
+    }
+    else if ($name == "" ){
+    
+        return false ;
+        
+    }
+    
+    }
+     neWname('traktoriukas');
+
 
 function emotion($emo){
-    //If($emo = )
+    If($emo == "happy"){
+        echo (" :)" );
+    }
+    else if ($emo == "sad"){
+        echo ( " :(" );
+    }else{
+        echo (":|");
+    }
 
 }
 
-emotion()
+
+
+name("Rima") ;
+echo (" is "); 
+emotion( "happy"); 
+echo (" today");
+
+
 ?>
-
-
-
-
-
